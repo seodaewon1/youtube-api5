5f12cd393a17b968b4339382524f1159538c365d
seodaewon1 <sdw10195@gmail.com>
2024-06-21 18:30:19 +0900

    0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './assets/scss/index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/scss/index.scss";
+import App from "./App";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+        <ToastContainer />
+    </React.StrictMode>
 );
